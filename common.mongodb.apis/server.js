4548be7fb5a1c64714396fc3f88370887ba5bf6a
Niyas K <niyas.k@Inatech.com>
2018-05-10 16:53:08 +0530

    fifth commit

---
+++ common.mongodb.apis/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const app = express();
+const cors = require('cors');
+const mongodb = require('./server/routes/mongodb/mongo.api');
+
+// Parsers
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false}));
+
+var originsWhitelist = [];
+var corsOptions = {
+    origin: function(origin, callback){
+          var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+          callback(null, isWhitelisted);
+    },
+    credentials:false
+}
+
+app.use(cors(corsOptions));
+// API location
+app.use('/mongodb', mongodb);
+
+//Set Port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () =>
+	{ console.log(`Running on localhost:${port}`);
+});
