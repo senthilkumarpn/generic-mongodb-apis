343671bd4b2c422221ce2c320472e00b9b65832f
Niyas K <niyas.k@Inatech.com>
2018-05-14 16:09:04 +0530

    folder renamed

--- server/routes/mongodb/model.mapping.js
+++ server/routes/mongodb/model.mapping.js
@@ -1,6 +1,6 @@
 var Products=require('../../Models/products.model');
 var Users = require('../../Models/users.model');
-
+// get future models here
 
 var ModelMapping = {
 
@@ -9,7 +9,8 @@ var ModelMapping = {
 
             case 'products': return Products;
             case 'users': return Users;
-            
+            // add cases for future models here
+
             default: return null;
         }
     }
